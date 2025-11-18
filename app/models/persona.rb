6ec915adf3ecdda4ce8229543fd056af220c389c
dan travin <dtravin@optonline.net>
2025-11-18 12:36:36 +0900

    codes for empty controllers and seeds

--- app/models/persona.rb
+++ app/models/persona.rb
@@ -2,6 +2,6 @@ class Persona < ApplicationRecord
   has_many :chats
 
   validates :name, presence: true, uniqueness: true
-  validates :system_prompt, presence: true
-  validates :content, presence: true
+  # validates :system_prompt, presence: true
+  # validates :content, presence: true
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -7,3 +7,22 @@
 #   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
+
+
+puts "Cleaning database..."
+
+User.destroy_all
+puts "Creating users..."
+haruna = User.create!(email: "haruna@gmail.com", password: "123456")
+steve = User.create!(email: "steve@gmail.com", password: "123456")
+dan = User.create!(email: "dan@gmail.com", password: "123456")
+puts "Created users."
+
+Persona.destroy_all
+puts "Creating personas..."
+Persona.create!(name: "Queen Elizabeth", category: "historical person")
+puts "Created Queen Elizabeth"
+Persona.create!(name: "Spongebob", category: "cartoon")
+puts "Created Spongebob"
+
+puts "Finished! Created #{Persona.count} personas."
