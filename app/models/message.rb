c365e78bfb81a460470314829192196e4cf0fc0d
Haruna Fujita <fujitaharuna.p@gmail.com>
2025-11-20 16:49:55 +0900

    added chat validations w/ button to start new chat

--- app/models/message.rb
+++ app/models/message.rb
@@ -3,4 +3,16 @@ class Message < ApplicationRecord
 
   validates :role, presence: true
   validates :content, presence: true
+
+  MAX_USER_MESSAGES = 10
+
+  validate :user_message_limit, if: -> { role == "user" }
+
+  private
+
+  def user_message_limit
+    if chat.messages.where(role: "user").count >= MAX_USER_MESSAGES
+      errors.add(:content, "You can only send #{MAX_USER_MESSAGES} messages per chat.")
+    end
+  end
 end
